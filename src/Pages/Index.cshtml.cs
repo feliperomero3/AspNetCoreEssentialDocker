305599c1913be40a01164dbfa1883bd0fff0c0d7
Felipe Romero <feliperomero3@outlook.com>
2021-12-15 23:53:14 -0600

    Preparing the Index Page.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using ExampleApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ExampleApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ProductRepository _repository;
+        private readonly IConfiguration _configuration;
         private readonly ILogger<IndexModel> _logger;
+        private readonly string _message;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ProductRepository repository, IConfiguration configuration, ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _repository = repository;
+            _configuration = configuration;
+            _message = configuration["MESSAGE"] ?? "Essential Docker for ASP.NET Core";
         }
 
-        public void OnGet()
-        {
+        public IList<Product> Products { get; set; }
 
+        public ActionResult OnGet()
+        {
+            ViewData["Message"] = _message;
+            Products = _repository.Products;
+
+            return Page();
         }
     }
 }
